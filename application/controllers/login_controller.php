7929f38a4d5fbd3f4d9aa54d59bb54709cf0dbc1
Dani <salcedochanza@gmail.com>
2021-05-15 10:06:00 +0200

    Solve api petitions

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -17,12 +17,27 @@ class login_controller  extends JwtAPI_Controller {
 
     public function login_post()
     {
+        $this->output->set_header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        $this->output->set_header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        $this->output->set_header("Access-Control-Allow-Origin: *");
+        $this->output->set_header("Authorization: Bearer");
+        
         $user = $this->post("user");
         $pass = $this->post("pass");
         if ($this->login($user, $pass)){
             $jwt = $this->renewJWT();
         }
     }
+
+    public function login_options() {
+        $this->output->set_header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        $this->output->set_header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        $this->output->set_header("Access-Control-Allow-Origin: *");
+        $this->output->set_header("Authorization: Bearer");
+
+        $this->response(null,200); // OK (200) being the HTTP response code
+    }
+
 }
 
 /* End of file login_controller.php */
